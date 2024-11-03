e641e702c352ae74496d54718c28b5faa2817360
parangat11 <parangataditya001@gmail.com>
2024-11-03 07:19:25 +0530

    Add font to the title.
    Webpack configurations managed for the same.

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,13 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      },      
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      }, 
     ],
   },
 };
